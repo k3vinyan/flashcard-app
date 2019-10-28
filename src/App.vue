adebb8ad9c47e677e71c4cb0e91d924d2b24f642
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2019-10-28 16:56:13 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="main-container">
+    <Card />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+   Card
   }
 }
 </script>
 
+<style src="./assets/css/w3.css" />
+
 <style>
+
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ol, ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +50,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+}
+
+.main-container {
+  display: flex;
+  justify-content: center;
+  height: 100vh;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card-container">
+        <div class="w3-green card">
+            Thiddfjdksjf
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "card",
+    data: function() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .card-container {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        height: 20%;
+    }
+
+    .card {
+        width: 90%;
+    }
+</style>
